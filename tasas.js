55867095b59ddfe99f35666b65e50b6453e080cd
ProjectTasaVDevs <oswaldo.devscode@gmail.com>
2025-12-23 11:55:28 -0400

    Create tasas.js

---
+++ tasas.js
@@ -0,0 +1,5 @@
+const baseDefaultRates = {
+    'USD': { value: 286.40, symbol: 'Bs', conversion: 285.40, displaySymbol: '$' },
+    'EUR': { value: 334.40, symbol: 'Bs', conversion: 334.40, displaySymbol: '€' },
+    'USDT': { value: 475.65, symbol: 'Bs', conversion: 475.65, displaySymbol: '₮' }
+};
