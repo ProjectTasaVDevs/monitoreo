31e27dc15d0e744461ee0de36d3774ad8872ce2e
ProjectTasaVDevs <oswaldo.devscode@gmail.com>
2026-01-07 20:29:40 -0400

    Sincronización de tasas: 07/01/2026 20:29

--- tasas.js
+++ tasas.js
@@ -1,5 +1,5 @@
 var tasas = {
     'USD': { value: 311.88, symbol: 'Bs', conversion: 311.88, displaySymbol: '$' },
     'EUR': { value: 364.83, symbol: 'Bs', conversion: 364.83, displaySymbol: '€' },
-    'USDT': { value: 830.26, symbol: 'Bs', conversion: 830.26, displaySymbol: '₮' }
+    'USDT': { value: 769.71, symbol: 'Bs', conversion: 769.71, displaySymbol: '₮' }
 };
